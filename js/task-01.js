283da28f0b7ec1d09a4defad0d13ff8a5fa4475b
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2021-09-10 09:23:07 +0300

    answer to the first task

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,22 @@
-const elemUl = document.querySelector("#categories");
-console.log('elemUl', elemUl);
+// Задание
+/*
+ * Напиши скрипт, который выполнит следующие операции.
+ * Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item. Получится 'В списке 3 категории.'.
+ * Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега h2) и количество элементов в категории (всех вложенных в него элементов li).
+ * Например для первой категории получится:
+ * Категория: Животные
+ * Количество элементов: 4
+ */
+
+const listCategories = document.querySelector("#categories").children;
+console.log(`В списке ${listCategories.length} категории`);
+
+// console.log(listCategories);
+
+for (let listItem of listCategories) {
+  console.log("Категория:", listItem.firstElementChild.textContent);
+  console.log(
+    "Количество элементов:",
+    listItem.querySelector("ul").children.length
+  );
+}
