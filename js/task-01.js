156c55cf2ddc7f66849e10dd616f19939443f697
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2021-09-14 09:48:56 +0300

    refactoring

--- js/task-01.js
+++ js/task-01.js
@@ -1,12 +1,12 @@
-// найти все категории списка
-const listCategoriesRef = document.querySelector("#categories").children;
-console.log(`В списке ${listCategoriesRef.length} категории`);
+// найти список категорий
+const listCategoriesEl = document.querySelector("#categories");
 
-// вывести результат парсинга категорий в консоль
-for (let listItem of listCategoriesRef) {
-  console.log("Категория:", listItem.firstElementChild.textContent);
-  console.log(
-    "Количество элементов:",
-    listItem.querySelector("ul").children.length
-  );
-}
+// найти все катекории в списке и вывести в консоль
+const liItemEl = listCategoriesEl.querySelectorAll(".item");
+console.log(`В списке ${liItemEl.length} категории`);
+
+// вывести в консоль все названия подкатегорий и кол-во элементов в них
+liItemEl.forEach((elem) => {
+  console.log(`Категория: ${elem.querySelector("h2").textContent}`);
+  console.log(`Количество элементов: ${elem.querySelectorAll("li").length}`);
+});
