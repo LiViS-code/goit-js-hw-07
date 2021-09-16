581ca5fd3df0f422fd799ad46e60d2a41d7354d2
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2021-09-16 16:34:21 +0300

    refactoring

--- js/task-08.js
+++ js/task-08.js
@@ -1,56 +1,51 @@
-// случайный цвет в rgb
-const randomColor = () => Math.floor(Math.random() * 256);
-
 // функция считывания значения от пользователя
-const inputValue = () => (amountDivs = Number(inputElem.value));
+const inputValue = () => (amountDivs = inputEl.value | 0);
 
 // функция создания div
-const createBoxes = function (amount) {
+const createBoxes = (amount) => {
   // размер певого div
   const sizeDiv = 30;
 
-  // фрагмент для вставки в DOM
-  const fragment = document.createDocumentFragment();
+  // массив для создаваемых div
+  const divBoxes = [];
 
-  // создать необходимое кол-во div
+  // создать указанное кол-во div
   for (let i = 1; i <= amount; i += 1) {
     // создать div
     const div = document.createElement("div");
 
     // добавить стили
-    div.style.backgroundColor = `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`;
+    div.style.backgroundColor = `#${Math.random().toString(16).slice(3, 9)}`;
     div.style.width = `${sizeDiv * i}px`;
-    div.style.height = `${sizeDiv * i}px`;
+    div.style.height = div.style.width;
 
-    // добавить новый div к фрагменту
-    fragment.append(div);
+    // сохранить созданный div в массив
+    divBoxes.push(div);
   }
 
   // внести изменения в DOM
-  divBoxesElem.append(fragment);
+  divBoxesEl.append(...divBoxes);
 };
 
 // функция удаления div
-const destroyBoxes = function () {
-  // удалять созданные div пока они есть у родителя
-  while (divBoxesElem.children.length > 0)
-    divBoxesElem.querySelector("div").remove();
+const destroyBoxes = () => {
+  divBoxesEl.innerHTML = "";
 };
 
 // сохранить место вставки для работы с div
-const divBoxesElem = document.querySelector("#boxes");
+const divBoxesEl = document.querySelector("#boxes");
 
 // найти поле ввода значений от пользователя
-const inputElem = document.querySelector("input[type = 'number']");
+const inputEl = document.querySelector("input[type = 'number']");
 
 // получить ввод от пользователя
 let amountDivs = 0; // значение по умолчанию - ни чего не делать
-inputElem.addEventListener("blur", inputValue.bind(inputElem));
+inputEl.addEventListener("blur", inputValue.bind(inputEl));
 
 // найти кнопки для действий пользователя
-const btnRenderElem = document.querySelector("button[data-action='render']");
-const btnDestroyElem = document.querySelector("button[data-action='destroy']");
+const btnRenderEl = document.querySelector("button[data-action='render']");
+const btnDestroyEl = document.querySelector("button[data-action='destroy']");
 
 // слушать нажатия кнопок
-btnRenderElem.addEventListener("click", () => createBoxes(amountDivs));
-btnDestroyElem.addEventListener("click", destroyBoxes);
+btnRenderEl.addEventListener("click", () => createBoxes(amountDivs));
+btnDestroyEl.addEventListener("click", destroyBoxes);
